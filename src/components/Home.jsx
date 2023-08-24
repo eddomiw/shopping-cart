5ed7f49739306f958e8f1a232a81797f409191af
Edgar Dominguez <eddomiw@gmail.com>
2023-08-23 20:15:08 -0700

    Create Home Page
    - ImageSlider and LatestReleases components make up home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>Home</div>;
+import ImageSlider from "./ImageSlider";
+import LatestReleases from "./LatestReleases";
+
+function Home() {
+  const imageData = [
+    "https://s7d1.scene7.com/is/image/scom/207_24_CTK_HPR_xl?$1500w$",
+    "https://s7d1.scene7.com/is/image/scom/207_HPR_24_OBK_xl?$1500w$",
+  ];
+  return (
+    <>
+      <ImageSlider images={imageData} />
+      <LatestReleases />;
+    </>
+  );
 }
+
+export default Home;
