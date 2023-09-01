dc017f83bedab56212d4201852dfaaf5252ca6ff
Edgar Dominguez <eddomiw@gmail.com>
2023-08-31 22:33:26 -0700

    Add totalCost state to equal total cost of items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,27 @@
 import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
 
 export default function Cart({ cartItems }) {
+  const [totalCost, setTotalCost] = useState();
+
+  const calculateTotalCost = () => {
+    let total = 0;
+    Object.keys(cartItems).forEach((itemId) => {
+      total += cartItems[itemId].quantity * cartItems[itemId].price;
+    });
+    return total;
+  };
+
+  const handleRemoveItem = (itemId) => {
+    // TODO: Implement item removal logic MAKE SURE IT IS THE CARTITEMS that is updated
+  };
+
+  // Update totalCost whenever cartItems change
+  useEffect(() => {
+    const calculatedTotalCost = calculateTotalCost();
+    setTotalCost(calculatedTotalCost);
+  }, [cartItems]);
+
   console.log(cartItems);
 
   return (
@@ -9,17 +30,24 @@ export default function Cart({ cartItems }) {
       <div>
         {Object.keys(cartItems).map((itemId) => (
           <div key={itemId}>
-            Item ID: {itemId},{" "}
-            <img className=" w-28 " src={cartItems[itemId].image}></img>{" "}
-            Quantity: {cartItems[itemId].quantity}, Price: $
-            {cartItems[itemId].price}
+            <img
+              className=" w-28 "
+              src={cartItems[itemId].image}
+              alt={itemId}
+            ></img>
+            Quantity: {cartItems[itemId].quantity}{" "}
+            <div>Price: ${cartItems[itemId].price}</div>
+            <button type="button" data-testid={`removeButton ${itemId}`}>
+              Remove
+            </button>
           </div>
         ))}
       </div>
+      <div>Total: ${totalCost}</div>
     </div>
   );
 }
 
 Cart.propTypes = {
-  cartItems: PropTypes.any.isRequired,
+  cartItems: PropTypes.object.isRequired,
 };
