a57f29249b38bc537ae167249235f24bca6341a1
Edgar Dominguez <eddomiw@gmail.com>
2023-09-05 19:28:12 -0700

    Add styles to buttons

--- src/components/Apparel.jsx
+++ src/components/Apparel.jsx
@@ -36,55 +36,65 @@ export default function Apparel({
   }, []);
 
   return (
-    <div className="flex  flex-col items-center  gap-2">
+    <div className="flex  flex-col items-center gap-2">
       <div className="mt-8 text-3xl">Apparel</div>
       {isLoading ? (
         <p>Loading...</p> //Displays loading indicator
       ) : error ? (
         <p>Error: {error.message}</p> //Displays error message if fetching failed
       ) : (
-        <ul className=" my-16 gap-10 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-10 text-xs">
+        <ul className=" my-16 gap-11 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-10 text-xs">
           {clothing.map((clothingItem) => (
             <li
               key={clothingItem.id}
-              className="flex flex-col items-center gap-1 justify-center"
+              className="flex flex-col items-center gap-2 justify-center"
             >
               <img
-                className="max-w-[350px] min-w-[170px] w-4/6 min-h-[170px] max-h-[350px] h-5/6 "
+                className="max-w-[350px] min-w-[170px] w-4/6 min-h-[170px] max-h-[275px] h-5/6 "
                 src={clothingItem.image}
                 alt={`${clothingItem.title} Image`}
               />
-              <div>{clothingItem.title}</div>
-              <div>{clothingItem.price}</div>
-              <div>Count: {itemCounts[clothingItem.id] || 0}</div>
-              <div>
+              <div className="h-8 flex justify-center items-center ">
+                {clothingItem.title}
+              </div>
+              <div>{`$${clothingItem.price.toFixed(2)}`}</div>
+
+              <div className="flex flex-col gap-4  items-center w-1/2">
+                <div className="flex border w-full border-l-black text-base">
+                  <button
+                    className="w-1/3"
+                    data-testid={`subtractButton ${clothingItem.id}`}
+                    onClick={() => decrementCount(clothingItem.id)}
+                  >
+                    -
+                  </button>
+                  <div className=" w-5/6 text-center">
+                    {itemCounts[clothingItem.id] || 0}
+                  </div>
+                  <button
+                    className="w-1/3"
+                    data-testid={`addButton ${clothingItem.id}`}
+                    onClick={() => incrementCount(clothingItem.id)}
+                  >
+                    +
+                  </button>
+                </div>
                 <button
-                  data-testid={`subtractButton ${clothingItem.id}`}
-                  onClick={() => decrementCount(clothingItem.id)}
+                  className="bg-black w-1/2 text-white p-2 rounded-lg focus:outline-none"
+                  onClick={() => {
+                    const quantity = itemCounts[clothingItem.id] || 0;
+                    addToCart(
+                      clothingItem.id,
+                      clothingItem.image,
+                      quantity,
+                      clothingItem.price,
+                      clothingItem.title
+                    );
+                  }}
                 >
-                  -
-                </button>
-                <button
-                  data-testid={`addButton ${clothingItem.id}`}
-                  onClick={() => incrementCount(clothingItem.id)}
-                >
-                  +
+                  Add to Cart
                 </button>
               </div>
-              <button
-                onClick={() => {
-                  const quantity = itemCounts[clothingItem.id] || 0;
-                  addToCart(
-                    clothingItem.id,
-                    clothingItem.image,
-                    quantity,
-                    clothingItem.price,
-                    clothingItem.title
-                  );
-                }}
-              >
-                Add to Cart
-              </button>
             </li>
           ))}
         </ul>
