a73d95b90a80815733095bba57fd543f3a42e624
Edgar Dominguez <eddomiw@gmail.com>
2023-09-06 19:55:58 -0700

    Display Navigation elements when in desktop version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header({ cartItems }) {
 
   return (
     <div className="flex justify-between w-full bg-pink-700 px-5 h-11 items-center text-black font-bold text-xl font-serif flex-shrink-0 ">
-      <button className="mr-auto basis-20" onClick={toggleNavigation}>
+      <button className="mr-auto md:hidden basis-20" onClick={toggleNavigation}>
         <img
           className="h-9"
           src="https://icon-library.com/images/hamburger-menu-icon-svg/hamburger-menu-icon-svg-17.jpg"
@@ -21,9 +21,29 @@ export default function Header({ cartItems }) {
         />
       </button>
       <Link to="/">
-        <h1 className="px-5 mx-auto">SWEATOO</h1>
+        <h1 className="px-5 mx-auto border-black border">SWEATOO</h1>
       </Link>
-      <Link to="cart" className="basis-20 flex text-center gap-1 px-5 ml-auto">
+      <ul className=" list-none p-0 text-black font-serif hidden md:flex md:w-1/2 md:justify-between">
+        <li className="p-3 text-center">
+          <Link to="/" className="no-underline">
+            Home
+          </Link>
+        </li>
+        <li className="p-3 text-center">
+          <Link to="/apparel" className="no-underline">
+            Apparel
+          </Link>
+        </li>
+        <li className="p-3 text-center">
+          <Link to="/account" className="no-underline">
+            Account
+          </Link>
+        </li>
+      </ul>
+      <Link
+        to="cart"
+        className="basis-20 flex text-center gap-1 px-5 ml-auto md:ml-0"
+      >
         <button>
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Shopping_cart_font_awesome.svg/1200px-Shopping_cart_font_awesome.svg.png"
